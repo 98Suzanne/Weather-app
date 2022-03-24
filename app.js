1bf48ef7ed4c4f96618bac623607e5ac067b99b4
98Suzanne <s.dinten@upcmail.nl>
2022-03-24 09:44:15 +0100

    Updated API key

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "1abad5c198be1cab987a15b09610f472";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -123,8 +123,8 @@ function displayWeather(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "1abad5c198be1cab987a15b09610f472";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
 }
 
@@ -141,10 +141,10 @@ form.addEventListener("submit", handleSubmit);
 
 function showPosition(position) {
   let units = "metric";
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "1abad5c198be1cab987a15b09610f472";
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeather);
 }
 
